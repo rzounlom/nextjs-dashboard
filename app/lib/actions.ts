0e1b9c6e5a65fff64e893ad0d1d414895c50e577
Romaric Zounlome <rzounlom@gmail.com>
2024-10-01 19:18:50 -0400

    complete auth flow

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,9 @@
 "use server";
 
+import { AuthError } from "next-auth";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
 import { z } from "zod";
 
@@ -118,5 +120,24 @@ export async function deleteInvoice(id: string) {
   } catch (error) {
     console.error("Database Error:", error);
     return { message: "Database Error: Failed to Delete Invoice." };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
